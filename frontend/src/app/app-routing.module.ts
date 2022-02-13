375df65791724ac740571dd4f2d8c8b39df9a9f8
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-02-13 19:48:04 +0100

    Removed listing-page component from the app component. Created all the components and put their routes into the app-routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Adding paths to the different pages
+  { path: '', redirectTo: '/listings', pathMatch: 'full' },
+  { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' },
+  { path: 'listings/:id', component: ListingDetailPageComponent },
+  { path: 'contact/:id', component: ContactPageComponent },
+  { path: 'new-listing', component: NewListingPageComponent },
+  { path: 'edit-listing/:id', component: EditListingPageComponent },
+  { path: 'my-listings', component: MyListingsPageComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
