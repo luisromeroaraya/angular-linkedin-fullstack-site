808c9258b08b17382cdfbbd8ca4ed412d00f1b16
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-02-23 16:07:50 +0100

    Create My Listings page

--- frontend/src/app/my-listings-page/my-listings-page.component.ts
+++ frontend/src/app/my-listings-page/my-listings-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
-  styleUrls: ['./my-listings-page.component.css']
+  styleUrls: ['./my-listings-page.component.css'],
 })
 export class MyListingsPageComponent implements OnInit {
-
-  constructor() { }
+  listings: Listing[] = [];
+  constructor() {}
 
   ngOnInit(): void {
+    this.listings = fakeMyListings;
   }
 
+  onDeleteClicked(listingId: String): void {
+    alert(`Deleting your listing with id: ${listingId}`);
+    // this.listings = this.listings.filter(listing => listing.id !== listingId);
+  }
 }
