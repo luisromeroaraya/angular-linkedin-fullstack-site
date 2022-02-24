397a754533076bc2881ba55b356d930cfe1402c9
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-02-24 18:11:20 +0100

    Created routes folder and getAllListings endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,21 @@
-import Hapi from '@hapi/hapi';
+import Hapi from "@hapi/hapi";
+import routes from "./routes";
 
 const start = async () => {
-    const server = Hapi.server({
-        port: 8000,
-        host: 'localhost'
-    });
+  const server = Hapi.server({
+    port: 8000,
+    host: "localhost",
+  });
 
-    server.route({
-        method: 'POST',
-        path: '/hello',
-        handler: (req, h) => {
-            const payload = req.payload;
-            const name = payload.name;
-            return `Hello ${name}!`;
-            // you can personalize returned codes like this: return.response('Hello!').code(200);
-        }
-    });
+  routes.forEach((route) => server.route(route));
 
-    await server.start();
-    console.log(`Server is listening on ${server.info.uri}`);
+  await server.start();
+  console.log(`Server is listening on ${server.info.uri}`);
 };
 
-process.on('unhandledRejection', err => {
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 
 start();
