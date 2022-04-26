f98717440e03ceafecf59b453de0039741762398
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-04-26 16:04:26 +0200

    Added deleteListing endpoint

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { getUserListingsRoute } from "./getUserListings";
 import { updateListingRoute } from "./updateListing";
+import { deleteListingRoute } from "./deleteListing";
 
 export default [
   addViewToListingRoute,
@@ -12,4 +13,5 @@ export default [
   getListingRoute,
   getUserListingsRoute,
   updateListingRoute,
+  deleteListingRoute,
 ];
