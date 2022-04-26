84cb68118275483b5f85449eb14fffbf671d4a05
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-04-26 16:00:53 +0200

    Added updateListing endpoint

---
+++ backend/src/routes/createNewListing.js
@@ -0,0 +1,14 @@
+import { v4 as uuid } from "uuid";
+import { db } from "../database";
+
+export const createNewListingRoute = {
+  method: "POST",
+  path: "/api/listings",
+  handler: async (req, h) => {
+    const { name = "", description = "", price = 0 } = req.payload;
+    const userId = "12345";
+    const views = 0;
+    await db.query(`INSERT INTO listings (name, description, price, user_id, views) VALUES (?, ?, ?, ?, ?);`,[name, description, price, userId, views]);
+    return { name, description, price, user_id: userId, views };
+  },
+};
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,15 @@
+import { addViewToListingRoute } from "./addViewToListing";
+import { createNewListingRoute } from "./createNewListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
-import { addViewToListingRoute } from "./addViewToListing";
 import { getUserListingsRoute } from "./getUserListings";
+import { updateListingRoute } from "./updateListing";
 
-export default [getAllListingsRoute, getListingRoute, addViewToListingRoute, getUserListingsRoute];
+export default [
+  addViewToListingRoute,
+  createNewListingRoute,
+  getAllListingsRoute,
+  getListingRoute,
+  getUserListingsRoute,
+  updateListingRoute,
+];
